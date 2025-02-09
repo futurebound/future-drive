67544b18b03b188936f6e49dc4b33b519a03677a
futurebound <mitchellm1618@gmail.com>
2025-02-09 10:20:44 -0800

    fix: update ids to bigint for singlestore

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,11 +1,49 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import { int, text, singlestoreTable } from "drizzle-orm/singlestore-core";
+import {
+  int,
+  text,
+  index,
+  singlestoreTableCreator,
+  bigint,
+} from "drizzle-orm/singlestore-core";
 import test from "node:test";
 
-export const users = singlestoreTable("users_table", {
-  id: int("id").primaryKey().autoincrement(),
-  name: text("name"),
-  age: int("age"),
-});
+// give DBs a prefix so can have multiple side project DBs easily managed together in singlestore free plan
+export const createTable = singlestoreTableCreator(
+  (name) => `future-drive_${name}`,
+);
+
+// using int id's because of MySWL world performance increase
+export const files = createTable(
+  "files_table",
+  {
+    id: bigint("id", { mode: "number", unsigned: true })
+      .primaryKey()
+      .autoincrement(),
+    name: text("name").notNull(),
+    size: int("size").notNull(),
+    url: text("url").notNull(),
+    parent: bigint("parent", { mode: "number", unsigned: true }).notNull(),
+  },
+  (t) => {
+    return [index("parent_index").on(t.parent)];
+    // add index on parent_index
+    // makes it easy to look up files based on parent folder id
+  },
+);
+
+export const folders = createTable(
+  "folders_table",
+  {
+    id: bigint("id", { mode: "number", unsigned: true })
+      .primaryKey()
+      .autoincrement(),
+    name: text("name").notNull(),
+    parent: bigint("parent", { mode: "number", unsigned: true }),
+  },
+  (t) => {
+    return [index("parent_index").on(t.parent)];
+  },
+);
